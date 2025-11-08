6738b730b4da3962438e91ca6e2521abfdb5f47b
Shazzaam <78475878+shazzaam7@users.noreply.github.com>
2025-11-08 21:45:16 +0100

    refactor: Moved `ControllerInformation` to be a `Control`

---
+++ source/DualSenseClient/Views/Controls/ControllerInformation.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DualSenseClient.Views.Controls;
+
+public partial class ControllerInformation : UserControl
+{
+    public ControllerInformation()
+    {
+        InitializeComponent();
+    }
+}
